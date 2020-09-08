9c7e5ba009f6a1e2ce535608b8c390c0174b66c4
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-09-08 14:26:34 -0400

    update

---
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UploadProductPage(){
+    return (
+        <div style={{maxWidth:'700px', margin:'2rem auto'}}>
+            faefeew
+        </div>
+    )
+}
+
+export default UploadProductPage
