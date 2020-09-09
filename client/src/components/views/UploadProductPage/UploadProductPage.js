98ffa254b3311f13525ce05596d07212add30215
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-09-09 19:03:01 -0400

    update

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const Continents=[
+    {key:1, value:"Africa"},
+    {key:2, value:"Europe"},
+    {key:3, value:"Asia"},
+    {key:4, value:"North America"},
+    {key:5, value:"South America"},
+    {key:6, value:"Australia"},
+    {key:7, value:"Antarctica"}
+]
 
 function UploadProductPage(){
 
-    const [state, setstate] = useState(initialState)
-    const onTitleChange=(event)=>{
+    const [TitleValue, setTitleValue] = useState("")
+    const [DescriptionValue, setDescriptionValue] = useState("")
+    const [PriceValue, setPriceValue] = useState(0)
+    const [ContinentValue, setContinentValue] = useState(1)
 
+    const onTitleChange=(event)=>{
+        setTitleValue(event.currentTarget.value)
+    }
+
+    const onDescriptionChange=(event)=>{
+        setDescriptionValue(event.currentTarget.value)
+    }
+
+    const onPriceChange=(event)=>{
+        setPriceValue(event.currentTarget.value)
+    } 
+
+    const onContinentsSelectChange=(event)=>{
+        setContinentValue(event.currentTarget.value)
     }
 
     return (
@@ -16,19 +42,19 @@ function UploadProductPage(){
                 <br/>
                 <br/>
                 <label>Title</label>
-                <input onChange={onTitleChange} value/>
+                <input onChange={onTitleChange} value={TitleValue}/>
                 <br/>
                 <br/>
                 <label>Description</label>
-                <textarea onChange value/>
+                <textarea onChange={onDescriptionChange} value={DescriptionValue}/>
                 <br/>
                 <br/>
                 <label>Price($)</label>
-                <textarea onChange value type="number"/>
-                <select>
-                    <option key value>
-
-                    </option>
+                <textarea onChange={onPriceChange} value={PriceValue} type="number"/>
+                <select onChange={onContinentsSelectChange}>
+                    {Continents.map(item=>(
+                        <option key={item.key} value={item.key}>{item.value}</option>
+                    ))}
                 </select>
                 <br/>
                 <br/>
