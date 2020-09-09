c9c1307197e1e0c2bf329d74000842248e1aa42f
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-09-08 20:28:59 -0400

    update

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,9 +1,39 @@
 import React from 'react'
 
 function UploadProductPage(){
+
+    const [state, setstate] = useState(initialState)
+    const onTitleChange=(event)=>{
+
+    }
+
     return (
         <div style={{maxWidth:'700px', margin:'2rem auto'}}>
-            faefeew
+            <div style={{textAlign:'center', marginBottom:'2rem'}}>
+                <h2>Upload Travel Product</h2>
+            </div>
+            <form onSubmit>
+                <br/>
+                <br/>
+                <label>Title</label>
+                <input onChange={onTitleChange} value/>
+                <br/>
+                <br/>
+                <label>Description</label>
+                <textarea onChange value/>
+                <br/>
+                <br/>
+                <label>Price($)</label>
+                <textarea onChange value type="number"/>
+                <select>
+                    <option key value>
+
+                    </option>
+                </select>
+                <br/>
+                <br/>
+                <button onClick>Submit</button>
+            </form>
         </div>
     )
 }
