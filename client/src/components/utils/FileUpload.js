65f8b4ea9d635b48a8546561a3585be0a33a57e1
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-09-10 23:19:51 -0400

    update

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,11 +1,33 @@
 import React from 'react'
 import Dropzone from 'react-dropzone'
 import {Icon} from 'antd'
+import { response } from 'express';
 
 function FileUpload() {
+
+    const onDrop=(files)=>{
+        let formData=new FormData();
+        const config={
+            header:{'content-type':'multipart/form-data'}
+        }
+        formData.append("file", files[0])
+
+        //save images into the Node Server
+        Axios.post('/api/product/uploadImage', formData, config)
+        .then(response=>{
+            if(response.data.success){
+
+            }else{
+                alert('Failed to save the Image in Server')
+            }
+        })
+
+        
+    }
+
     return (
         <div style={{display:'flex', justifyContent:'space-between'}}>
-            <Dropzone onDrop multiple maxSize>
+            <Dropzone onDrop={onDrop} multiple maxSize>
                 {({getRootProps, getInputProps})=>(
                     <div style={{width:'300px', height:'240px', border:'1px solid lightgray', display:'flex', alignItems:'center', justifyContent:'center'}}
                         {...getRootProps()}
