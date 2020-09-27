02b81493c94a132d2f13930885107d8b1e8efd83
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-09-27 13:58:42 -0400

    update

--- client/src/components/views/CartPage/CartPage.js
+++ client/src/components/views/CartPage/CartPage.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
-import {getCartItems} from '../../../_actions/user_actions'
+import { getCartItems } from '../../../_actions/user_actions'
+import UserCardBlock from './Sections/UserCardBlock'
+import { Result } from 'antd'
 
 function CartPage(props) {
+
     const dispatch = useDispatch()
+
     useEffect(() => {
         let cartItems = []
 
@@ -15,11 +19,18 @@ function CartPage(props) {
                 dispatch(getCartItems(cartItems, props.user.userData.cart))
             }
         }
-    }, [])
+    }, [props.user.userData])
 
     return (
-        <div>
-            CartPage
+        <div style={{ width: '85%', margin: '3rem auto' }} >
+            <h1>My Cart</h1>
+            <div>
+                <UserCardBlock />
+                <div style={{ marginTop: '3rem' }} >
+                    <h2>Total amount: $</h2>
+                </div>
+                <Result status="success" title="Successfully Purchased Items" />
+            </div>
         </div>
     )
 }
