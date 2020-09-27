2f9cda294ace2fa772a2399bdeac70e553ebd3cf
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2020-09-27 16:06:40 -0400

    update

--- client/src/components/views/CartPage/Sections/UserCardBlock.js
+++ client/src/components/views/CartPage/Sections/UserCardBlock.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-function UserCardBlock() {
+function UserCardBlock(props) {
+
+    const renderItems = () => {
+
+        props.products && props.products.map(product => (
+            <tr key={product._id} >
+                <td>
+                    <img style={{ width: '70px' }} alt="product" src />
+                </td>
+                <td>{product.quantity} EA</td>
+                <td>$ {product.price}</td>
+                <td><button onClick>Remove</button></td>
+            </tr>
+        ))
+    }
+    
+    useEffect(() => {
+        renderItems()
+    }, [props.products])
+
     return (
         <div>
             <table>
@@ -12,6 +31,9 @@ function UserCardBlock() {
                         <th>Remobe from Cart</th>
                     </tr>
                 </thead>
+                <tbody>
+                    {renderItems()}
+                </tbody>
             </table>
         </div>
     )
