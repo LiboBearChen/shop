f56cbfe4fab8b9915681c2b03e755336d3d10d16
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-01-05 20:39:39 -0500

    update

--- client/src/components/views/DetailProductPage/Sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/Sections/ProductInfo.js
@@ -9,23 +9,38 @@ function ProductInfo(props) {
         setProduct(props.detail)
     }, [props.detail])
 
-    const addToCartHandler=()=>{
+    const addToCartHandler = () => {
         props.addToCart(props.detail._id)
     }
 
     return (
         <div>
-            <Descriptions title="Product Info" >
-                <Descriptions.Item label="Price" >{Product.price}</Descriptions.Item>
-                <Descriptions.Item label="Sold" >{Product.sold}</Descriptions.Item>
-                <Descriptions.Item label="View" >{Product.views}</Descriptions.Item>
-                <Descriptions.Item label="Description" >{Product.description}</Descriptions.Item>
-            </Descriptions>
-            <br />
-            <br />
-            <br />
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Button size="large" shape="round" type="danger" onClick={addToCartHandler} >Add to Cart</Button>
+            <h4>Details</h4>
+            <ul className="nav nav-pills">
+                <li className="active"><a data-toggle="pill" href="#home">Description</a></li>
+                <li><a data-toggle="pill" href="#menu1">Menu 1</a></li>
+                <li><a data-toggle="pill" href="#menu2">Menu 2</a></li>
+                <li><a data-toggle="pill" href="#menu3">Price</a></li>
+            </ul>
+            <div className="tab-content">
+                <div id="home" className="tab-pane fade in active show">
+                    <p>{Product.description}</p>
+                </div>
+                <div id="menu1" className="tab-pane fade">
+                    <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                </div>
+                <div id="menu2" className="tab-pane fade">
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                </div>
+                <div id="menu3" className="tab-pane fade">
+                    <p>{Product.price} $</p>
+                    <br />
+                    <br />
+                    <br />
+                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Button size="large" shape="round" type="danger" onClick={addToCartHandler} >Add to Cart</Button>
+                    </div>
+                </div>
             </div>
         </div>
     )
