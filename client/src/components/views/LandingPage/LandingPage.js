5ed9e6d44ea983bc711a99d2b7d269a9398688f9
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-01-10 14:33:08 -0500

    update

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -12,9 +12,65 @@ function ProductPage() {
 
     return (
         <div className='container'>
-            <ImageSlider images={banners} style={{ width: '100%', height: '300px' }}/>
-        </div>
+            <ImageSlider images={banners} style={{ width: '100%', height: '300px' }} />
+            <div style={{textAlign: 'center'}}>
+                <h1>Find Solutions for Your ...</h1>
+                <h4>...</h4>
+                <div style={{ textAlign: 'center'}}>
+                    <div className="row " style={{ marginBottom: '20px' }}>
+                        <div className="col">
+                            <div className="card mx-auto" style={{ width: '18rem' }}>
+                                <img className="card-img-top" src={`http://localhost:5000/uploads/cards/card1.jpg`} alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Stock market analysis website</h5>
+                                    <p className="card-text"><strong>Tech stack:</strong></p>
+                                    <p className="card-text">ReactJS</p>
+                                    <br />
+                                    <a href="/" className="btn btn-primary">Check it</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card mx-auto" style={{ width: '18rem' }}>
+                                <img className="card-img-top" src={`http://localhost:5000/uploads/cards/card2.jpg`} alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Webpage analysis chrome extension</h5>
+                                    <p className="card-text"><strong>Tech stack:</strong></p>
+                                    <p className="card-text">jQuery</p>
+                                    <br />
+                                    <a href="/" className="btn btn-primary">Check it</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col">
+                            <div className="card mx-auto" style={{ width: '18rem' }}>
+                                <img className="card-img-top" src={`http://localhost:5000/uploads/cards/card1.jpg`} alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Digital product shopping website</h5>
+                                    <p className="card-text"><strong>Tech stack:</strong></p>
+                                    <p className="card-text">MERN(MongoDB, Express, React and Node.js)</p>
+                                    <a href="/" className="btn btn-primary">Check it</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row ">
+                        <div className="col">
+                            <div className="card mx-auto" style={{ width: '18rem' }}>
+                                <img className="card-img-top" src={`http://localhost:5000/uploads/cards/card2.jpg`} alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">Coding blog (this website)</h5>
+                                    <p className="card-text"><strong>Tech stack:</strong></p>
+                                    <p className="card-text">Flask</p>
+                                    <a href="/" className="btn btn-primary">Check it</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+            </div>
+        </div>
     )
 }
 
