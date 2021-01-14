ccbf955a7f9bb6a58198c22377cec60a6a07ed74
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-01-13 22:32:53 -0500

    update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import Auth from "../hoc/auth";
 
@@ -9,13 +9,11 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
-import UploadProductPage from "./views/UploadProductPage/UploadProductPage"
-import DetailProductPage from "./views/DetailProductPage/DetailProductPage"
-import CartPage from './views/CartPage/CartPage'
-import HistoryPage from './views/HistoryPage/HistoryPage'
-import AboutPage from './views/AboutPage/AboutPage'
-
-
+import UploadProductPage from "./views/UploadProductPage/UploadProductPage";
+import DetailProductPage from "./views/DetailProductPage/DetailProductPage";
+import CartPage from "./views/CartPage/CartPage";
+import HistoryPage from "./views/HistoryPage/HistoryPage";
+import AboutPage from "./views/AboutPage/AboutPage";
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -23,16 +21,24 @@ import AboutPage from './views/AboutPage/AboutPage'
 
 function App() {
   return (
-    <Suspense fallback={(<div>Loading...</div>)}>
+    <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+      <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/product" component={Auth(ProductPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/product/upload" component={Auth(UploadProductPage, true)} />
-          <Route exact path="/product/:productID" component={Auth(DetailProductPage, true)} />
+          <Route
+            exact
+            path="/product/upload"
+            component={Auth(UploadProductPage, true)}
+          />
+          <Route
+            exact
+            path="/product/:productID"
+            component={Auth(DetailProductPage, null)}
+          />
           <Route exact path="/user/cart" component={Auth(CartPage, true)} />
           <Route exact path="/history" component={Auth(HistoryPage, true)} />
           <Route exact path="/about" component={Auth(AboutPage, null)} />
--- client/src/components/views/ProductPage/Sections/Datas.js
+++ client/src/components/views/ProductPage/Sections/Datas.js
@@ -1,22 +1,15 @@
+const continents = [
+  { _id: 1, name: "Illustration" },
+  { _id: 2, name: "MOD" },
+  { _id: 3, name: "Fiction" },
+  { _id: 4, name: "Maps" },
+];
 
+const price = [
+  { _id: 0, name: "Any", array: [] },
+  { _id: 1, name: "$0 to $199", array: [0, 1.99] },
+  { _id: 2, name: "$200 to $249", array: [2, 4.99] },
+  { _id: 3, name: "$250 to $279", array: [5, 9.99] },
+];
 
-const continents=[
-    {"_id":1, "name":"Africa"},
-    {"_id":2, "name":"Europe"},
-    {"_id":3, "name":"Asia"},
-    {"_id":4, "name":"North America"},
-    {"_id":5, "name":"South America"},
-    {"_id":6, "name":"Australia"},
-    {"_id":7, "name":"Antarctica"}
-]
-
-const price=[
-    {"_id":0, "name":"Any", "array":[]},
-    {"_id":1, "name":"$0 to $199", "array":[0,199]},
-    {"_id":2, "name":"$200 to $249", "array":[200,249]},
-    {"_id":3, "name":"$250 to $279", "array":[250,279]},
-    {"_id":4, "name":"$280 to $299", "array":[280,299]},
-    {"_id":5, "name":"More than $300", "array":[300,88888888]}
-]
-
-export{continents,price}
+export { continents, price };
