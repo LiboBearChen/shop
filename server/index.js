115a42d9607c8169877005ee05f07ef16f7b48c7
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-01-22 21:48:09 -0500

    update

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
+//Required External Modules
 const express = require("express");
-const app = express();
-const path = require("path");
 const cors = require("cors");
+const helmet = require("helmet");
+
+const { clientOrigins, serverPort } = require("./config/env.dev");
+const { messagesRouter } = require("./messages/messages.router");
+
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const mongoose = require("mongoose");
+
+//App Variables
+const app = express();
+const apiRouter = express.Router();
+const path = require("path");
+
+//App Configuration
+app.use(helmet());
+app.use(cors({ origin: clientOrigins }));
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+apiRouter.use("/messages", messagesRouter);
 
 const connect = mongoose
   .connect(config.mongoURI, {
@@ -15,8 +33,6 @@ const connect = mongoose
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.use(cors());
-
 //to not get any deprecation warning or error
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,6 +40,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/product", require("./routes/product"));
@@ -44,8 +61,8 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+//Server Activation
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
   console.log(`Server Listening on ${port}`);
 });
