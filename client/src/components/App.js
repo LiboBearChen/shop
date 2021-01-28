cc693ff583f5275dd5f1617ae1dd3ca3ef347691
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-01-27 21:40:41 -0500

    update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,10 @@
 import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import ProtectedRoute from "../auth/protected-route";
-import Auth from "../hoc/auth";
 
 // views
 import LandingPage from "./views/LandingPage/LandingPage.js";
 import ProductPage from "./views/ProductPage/ProductPage.js";
-import LoginPage from "./views/LoginPage/LoginPage.js";
-import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
 import UploadProductPage from "./views/UploadProductPage/UploadProductPage";
@@ -18,36 +15,29 @@ import AboutPage from "./views/AboutPage/AboutPage";
 import Profile from "./views/ProfilePage/profile";
 import ExternalApi from "./views/ExternalApiPage/external-api";
 
-//null   Anyone Can go inside
-//true   only logged in user can go inside
-//false  logged in user can't go inside
-
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
       <div style={{ paddingTop: "69px", minHeight: "calc(100vh - 80px)" }}>
         <Switch>
-          {/* <Route exact path="/" component={Auth(LandingPage, null)} /> */}
           <Route exact path="/" component={LandingPage} />
           <Route exact path="/product" component={ProductPage} />
           <ProtectedRoute path="/profile" component={Profile} />
           <ProtectedRoute path="/external-api" component={ExternalApi} />
-          {/* <Route exact path="/login" component={Auth(LoginPage, false)} />
-          <Route exact path="/register" component={Auth(RegisterPage, false)} /> */}
-          <Route
+          <ProtectedRoute
             exact
             path="/product/upload"
-            component={Auth(UploadProductPage, true)}
+            component={UploadProductPage}
           />
           <Route
             exact
             path="/product/:productID"
             component={DetailProductPage}
           />
-          <Route exact path="/user/cart" component={Auth(CartPage, true)} />
-          <Route exact path="/history" component={Auth(HistoryPage, true)} />
-          <Route exact path="/about" component={Auth(AboutPage, null)} />
+          <ProtectedRoute exact path="/user/cart" component={CartPage} />
+          <ProtectedRoute exact path="/history" component={HistoryPage} />
+          <Route exact path="/about" component={AboutPage} />
         </Switch>
       </div>
       <Footer />
