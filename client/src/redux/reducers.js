a4d6a89c8eede1e61a20caf17df16648568f09ef
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-02-09 22:47:10 -0500

    update

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,26 +1,56 @@
-import ACTIONS from "./actions";
+import {
+  LOGIN_USER,
+  REGISTER_USER,
+  AUTH_USER,
+  LOGOUT_USER,
+  ADD_TO_CART_USER,
+  GET_CART_ITEMS_USER,
+  REMOVE_CART_ITEM_USER,
+  ON_SUCCESS_BUY_USER,
+} from "./actions";
 
-const initialState = {
-  contact: {
-    name: "",
-    number: "",
-    contactAdded: false,
-  },
-};
-
-function contactDataReducer(state = initialState, action) {
+export default function (state = {}, action) {
   switch (action.type) {
-    case ACTIONS.Types.ADD_CONTACT:
-      return Object.assign({}, state, {
-        contact: { ...action.contact },
-      });
-    case ACTIONS.Types.RESET_CONTACT:
-      return Object.assign({}, state, {
-        contact: initialState,
-      });
+    case REGISTER_USER:
+      return { ...state, register: action.payload };
+    case LOGIN_USER:
+      return { ...state, loginSucces: action.payload };
+    case AUTH_USER:
+      return { ...state, userData: action.payload };
+    case LOGOUT_USER:
+      return { ...state };
+    case ADD_TO_CART_USER:
+      return {
+        ...state,
+        userData: {
+          ...state.userData,
+          cart: action.payload,
+        },
+      };
+    case GET_CART_ITEMS_USER:
+      return {
+        ...state,
+        cartDetail: action.payload,
+      };
+    case REMOVE_CART_ITEM_USER:
+      return {
+        ...state,
+        cartDetail: action.payload.cartDetail,
+        userData: {
+          ...state.userData,
+          cart: action.payload.cart,
+        },
+      };
+    case ON_SUCCESS_BUY_USER:
+      return {
+        ...state,
+        userData: {
+          ...state.userData,
+          cart: action.payload.cart,
+        },
+        cartDetail: action.payload.cartDetail,
+      };
     default:
       return state;
   }
 }
-
-export default contactDataReducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,3 +2,12 @@ import { createStore } from "redux";
 import contactDataReducer from "./reducers";
 
 export default createStore(contactDataReducer);
+/////////////////////////////////////////////////////////
+import { combineReducers } from "redux";
+import user from "./user_reducer";
+
+const rootReducer = combineReducers({
+  user,
+});
+
+export default rootReducer;
