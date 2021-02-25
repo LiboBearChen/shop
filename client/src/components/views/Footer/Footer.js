141926166cee30c6ff444f48a9334350841a76d5
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-02-24 22:53:21 -0500

    update

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,16 +1,38 @@
-import React from 'react'
-
+import React from "react";
 
 function Footer() {
-    return (
-        <div style={{
-            height: '80px', display: 'flex',
-            flexDirection: 'column', alignItems: 'center',
-            justifyContent: 'center', fontSize:'1rem'
-        }}>
-           <a href="/about"><p>About This Website</p></a>
-        </div>
-    )
+  return (
+    <div className="row">
+      <div className="col" key="img">
+        <a href="/about">
+          <p>About This Website</p>
+        </a>
+        <a href="/about">
+          <p>About This Website</p>
+        </a>
+        <a href="/about">
+          <p>About This Website</p>
+        </a>
+        <a href="/about">
+          <p>About This Website</p>
+        </a>
+      </div>
+      <div className="col" key="info">
+        <a href="/about">
+          <p>About This Website</p>
+        </a>
+        <a href="/about">
+          <p>About This Website</p>
+        </a>
+        <a href="/about">
+          <p>About This Website</p>
+        </a>
+        <a href="/about">
+          <p>About This Website</p>
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
