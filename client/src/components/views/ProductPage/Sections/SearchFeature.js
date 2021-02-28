dae4dc8e2162e27b6cd38d13c64f24fcf4bbe1c0
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-02-27 20:37:52 -0500

    update

--- client/src/components/views/ProductPage/Sections/SearchFeature.js
+++ client/src/components/views/ProductPage/Sections/SearchFeature.js
@@ -2,29 +2,47 @@ import React, { useState } from "react";
 import Autocomplete from "@material-ui/lab/Autocomplete";
 
 const SearchFeature = (props) => {
+  const [chosen, setChosen] = useState(false);
+  const [url, setUrl] = useState("");
+
   const onChange = (e, selectedOption) => {
-    products.forEach((product) => {
-      if (product.name === selectedOption) {
+    let product;
+    products.forEach((p) => {
+      if (p.name === selectedOption) {
+        product = p;
       }
     });
 
-    selectedOption ? setState() : setState();
+    if (selectedOption) {
+      setChosen(true);
+      setUrl(`/product/${product._id}`);
+    }
   };
+
   return (
-    <Autocomplete
-      options={props.productNames}
-      getOptionLabel={(option) => option}
-      style={{ width: 300 }}
-      onChange={onChange}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="available products"
-          variant="outlined"
-          fullWidth
-        />
+    <div>
+      <Autocomplete
+        options={props.productNames}
+        getOptionLabel={(option) => option}
+        style={{ width: 300 }}
+        onChange={onChange}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="available products"
+            variant="outlined"
+            fullWidth
+          />
+        )}
+      />
+      {chosen ? (
+        <div>
+          <a href={url}>Go To This Product</a>
+        </div>
+      ) : (
+        <div>Search For A Product</div>
       )}
-    />
+    </div>
   );
 };
 
